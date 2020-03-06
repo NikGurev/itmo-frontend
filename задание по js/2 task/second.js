4f9e5208a85a012ac8189b3bcbfcf4e3438f1886
NGurev <Nikita.Gurev@lanit-tercom.com>
2020-03-06 16:01:42 +0300

    Создал метод для вывода значения инпута

--- задание по js/2 task/second.js
+++ задание по js/2 task/second.js
@@ -1,5 +1,30 @@
 "use strict";
 
+var form = document.createElement('form'),
+    input = document.createElement('input'),
+    label = document.createElement('label'),
+    button = document.createElement('button')
+    ;
+
+
+input.type = 'text';
+input.id = 'cols';
+
+label.innerText = 'Количество столбцов';
+label.htmlFor = 'cols';
+form.appendChild(label);
+form.appendChild(input);
+
+input = document.createElement('input');
+
+button.type = 'button';
+button.innerText = 'тест';
+button.onclick = () => console.log(document.getElementById('cols').value);
+form.appendChild(button);
+document.body.appendChild(form);
+
+
+
 
 
 
