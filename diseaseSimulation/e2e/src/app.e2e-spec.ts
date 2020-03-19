2051260a03d1a135afe1c0202039b5bb044b46a6
NGurev <Nikita.Gurev@lanit-tercom.com>
2020-03-19 23:59:55 +0300

    restructured app

--- diseaseSimulation/e2e/src/app.e2e-spec.ts
+++ diseaseSimulation/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('disease_simulation app is running!');
+    expect(page.getTitleText()).toEqual('diseaseSimulation app is running!');
   });
 
   afterEach(async () => {
--- diseaseSimulation/karma.conf.js
+++ diseaseSimulation/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/disease_simulation'),
+      dir: require('path').join(__dirname, './coverage/diseaseSimulation'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
